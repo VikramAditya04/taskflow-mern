280eaf4d6af4c27ada0c7097c33680e95b6f9f94
Vikram Aditya <vikramaadi9798@gmail.com>
2026-05-01 20:06:25 +0530

    fix: dynamic API URL

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+
 import connectDB from './config/mongodb.js';
 import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
@@ -9,24 +10,53 @@ import taskRoutes from "./routes/taskRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
+// Connect DB
 connectDB();
 
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173']
-app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}))
-app.use(express.json());
+//  Allowed Origins
+const allowedOrigins = [
+process.env.FRONTEND_URL,   
+"http://localhost:5173"    
+];
 
-// API Endpoints
-app.get('/', (req, res)=> res.send("API Working"));
+// CORS Setup 
+app.use(cors({
+origin: function (origin, callback) {
+// allow requests with no origin 
+if (!origin) return callback(null, true);
+
+```
+if (allowedOrigins.includes(origin)) {
+  return callback(null, true);
+} else {
+  return callback(new Error("Not allowed by CORS"));
+}
+```
+
+},
+credentials: true
+}));
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Health check
+app.get('/', (req, res) => {
+res.send("API Working ");
+});
+
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/users", userRoutes);
 
-
-app.listen(port, ()=> console.log(`Server is running on PORT:${port}`));
+// Start server
+app.listen(port, () => {
+console.log(`Server running on PORT: ${port}`);
+});
